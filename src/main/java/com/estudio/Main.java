a1b39aa92640216b07b6650971498125b6ca3851
Yang Jian <jyang@digby.com>
2013-03-24 01:31:45 -0500

    Inital Import.

---
+++ src/main/java/com/estudio/Main.java
@@ -0,0 +1,28 @@
+/*
+ * -----------------------------------------------------------------
+ * Copyright (c) 2012 EStudio, All Rights Reserved.
+ * This software is the proprietary information of EStudio,
+ * Use is subject to strict licensing terms.
+ * -----------------------------------------------------------------
+ */
+package com.estudio;
+
+import com.estudio.utils.log.L;
+
+/**
+ * @author yang
+ * @version 1.0 - Sep 26, 2012
+ *
+ */
+public class Main
+{
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+    	L.info(Main.class, "Logger works fine.");
+    }
+
+}
---
+++ src/test/java/com/estudio/MainTest.java
@@ -0,0 +1,30 @@
+/*
+ * -----------------------------------------------------------------
+ * Copyright (c) 2012 EStudio, All Rights Reserved.
+ * This software is the proprietary information of EStudio,
+ * Use is subject to strict licensing terms.
+ * -----------------------------------------------------------------
+ */
+package com.estudio;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author yang
+ * @version 1.0 - Sep 26, 2012
+ *
+ */
+public class MainTest
+{
+
+    @Test
+    public void test()
+    {
+    	new Main();
+    	Main.main(null);
+        assertTrue("Dummy test", true);
+    }
+
+}
